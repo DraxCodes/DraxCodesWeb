9cdbe9cb0898039c16ecf9c5c1eb2408769986c3
Joel Parkinson <joelparkinson@live.co.uk>
2019-05-27 05:47:44 +0100

    Updates

---
+++ DraxCodesWeb/Services/TodoJsonStorage.cs
@@ -0,0 +1,22 @@
+﻿using DraxCodesWeb.Services.Entities;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DraxCodesWeb.Services
+{
+    public class TodoJsonStorage
+    {
+        public string ConvertJson(object obj)
+        {
+            return JsonConvert.SerializeObject(obj, Formatting.Indented);
+        }
+
+        public List<TodoItem> LoadItems(string path)
+        {
+            var json = File.ReadAllText(path);
+            if(string.IsNullOrWhiteSpace(json)) { return null; }
+            return JsonConvert.DeserializeObject<List<TodoItem>>(json);
+        }
+    }
+}
--- DraxCodesWeb/Startup.cs
+++ DraxCodesWeb/Startup.cs
@@ -1,3 +1,4 @@
+using DraxCodesWeb.Services;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,7 @@ namespace DraxCodesWeb
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<TodoJsonStorage>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
